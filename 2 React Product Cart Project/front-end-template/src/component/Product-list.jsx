214b2df155c2701ff2da54e8a1216117f25f5817
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2023-07-21 02:48:26 -0700

    Front End Template Ready

---
+++ 2 React Product Cart Project/front-end-template/src/component/Product-list.jsx
@@ -0,0 +1,71 @@
+const ProductList = () => {
+    return (
+        <div className="container z-10 mx-auto my-12 p-9">
+            <div className="grid grid-cols-1 mt-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+
+                    <div className="card w-100 bg-white shadow-xl">
+                        <figure><img src="https://static-01.daraz.com.bd/p/b802b8ebb0a784ec791caa75e2d4de66.jpg" alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h6 className="text-black">iPhone 14 Pro Max iOS 16 6.7 inches Super Retina</h6>
+                            <p className="text-sm text-gray-400">If a dog chews shoes whose shoes does he choose?</p>
+                            <h6 className="font-bold">Price: $1,000</h6>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-sm btn-outline btn-primary">Add Cart</button>
+                            </div>
+                        </div>
+                    </div>
+
+                <div className="card w-100 bg-white shadow-xl">
+                    <figure><img src="https://static-01.daraz.com.bd/p/b802b8ebb0a784ec791caa75e2d4de66.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h6 className="text-black">iPhone 14 Pro Max iOS 16 6.7 inches Super Retina</h6>
+                        <p className="text-sm text-gray-400">If a dog chews shoes whose shoes does he choose?</p>
+                        <h6 className="font-bold">Price: $1,000</h6>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-sm btn-outline btn-primary">Add Cart</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card w-100 bg-white shadow-xl">
+                    <figure><img src="https://static-01.daraz.com.bd/p/b802b8ebb0a784ec791caa75e2d4de66.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h6 className="text-black">iPhone 14 Pro Max iOS 16 6.7 inches Super Retina</h6>
+                        <p className="text-sm text-gray-400">If a dog chews shoes whose shoes does he choose?</p>
+                        <h6 className="font-bold">Price: $1,000</h6>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-sm btn-outline btn-primary">Add Cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-100 bg-white shadow-xl">
+                    <figure><img src="https://static-01.daraz.com.bd/p/b802b8ebb0a784ec791caa75e2d4de66.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h6 className="text-black">iPhone 14 Pro Max iOS 16 6.7 inches Super Retina</h6>
+                        <p className="text-sm text-gray-400">If a dog chews shoes whose shoes does he choose?</p>
+                        <h6 className="font-bold">Price: $1,000</h6>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-sm btn-outline btn-primary">Add Cart</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card w-100 bg-white shadow-xl">
+                    <figure><img src="https://static-01.daraz.com.bd/p/b802b8ebb0a784ec791caa75e2d4de66.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h6 className="text-black">iPhone 14 Pro Max iOS 16 6.7 inches Super Retina</h6>
+                        <p className="text-sm text-gray-400">If a dog chews shoes whose shoes does he choose?</p>
+                        <h6 className="font-bold">Price: $1,000</h6>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-sm btn-outline btn-primary">Add Cart</button>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    );
+};
+export default ProductList;
